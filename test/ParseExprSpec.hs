6315735a9736c4340bb4818f14485e6efec1e296
Yury Kotlyarov <yura@brainhouse.ru>
2019-11-16 11:01:03 +0700

    test: Test for parseExpr and for parseMacro

---
+++ test/ParseExprSpec.hs
@@ -0,0 +1,28 @@
+module ParseExprSpec (main, spec) where
+
+import Test.Hspec
+import Test.Hspec.Parsec
+import Text.Parsec
+
+import Lib
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "parseExpr" $ do
+    let parseExpr' = parse parseExpr ""
+
+    it "parses a simple string" $ do
+      parseExpr' "\"hello\"" `shouldParse` (String "hello")
+
+    it "parses boolean true value (#t)" $ do
+      parseExpr' "#t" `shouldParse` (Bool True)
+
+    it "parses boolean false value (#f)" $ do
+      parseExpr' "#f" `shouldParse` (Bool False)
+
+    it "parses number value in bin format (with #b prefix)" $ do
+      parseExpr' "#b1101" `shouldParse` (Number 13)
+
---
+++ test/ParseMacroSpec.hs
@@ -0,0 +1,34 @@
+module ParseMacroSpec (main, spec) where
+
+import Test.Hspec
+import Test.Hspec.Parsec
+import Text.Parsec
+
+import Lib
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "parseMacro" $ do
+    let parseMacro' = parse parseMacro ""
+
+    it "parses boolean true value" $ do
+      parseMacro' "#t" `shouldParse` (Bool True)
+
+    it "parses boolean false value" $ do
+      parseMacro' "#f" `shouldParse` (Bool False)
+
+    it "parses number in binary format" $ do
+      parseMacro' "#b1101" `shouldParse` Number 13
+
+    it "parses number in octal format" $ do
+      parseMacro' "#o1101" `shouldParse` Number 577
+
+    it "parses number in decimal format" $ do
+      parseMacro' "#d1101" `shouldParse` Number 1101
+
+    it "parses number in decimal format without prefix" $ do
+      parseMacro' "#xff" `shouldParse` Number 255
+
